13c7c7948eacda3249cb51e1afa751bd32c208d1
Dinononoo <127750636+Dinononoo@users.noreply.github.com>
2025-01-29 16:07:56 +0700

    first commit

--- config/app.php
+++ config/app.php
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://10.1.216.160'),
 
     /*
     |--------------------------------------------------------------------------
